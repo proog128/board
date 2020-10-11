2b956a987c835fd00acc01d7d899be7edd3ee4cc
Tobias Haeussler <mail@tobiashaeussler.com>
2020-10-11 18:27:29 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use('/:board', express.static(path.join(__dirname, 'public')));
+app.use('/', (req, res) => {
+    res.redirect('/default');
+});
+
+io.on('connect', (socket) => {
+    const boardName = socket.handshake.query.board;
+    socket.join(boardName);
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('additem', (item, cell, sibling) => {
+        io.in(boardName).emit('additem', item, cell, sibling);
+    });
+    socket.on('updateitemcontent', (item, content) => {
+        io.in(boardName).emit('updateitemcontent', item, content);
+    });
+    socket.on('updateitemcolor', (item, color) => {
+        io.in(boardName).emit('updateitemcolor', item, color);
+    });
+    socket.on('deleteitem', (item) => {
+        io.in(boardName).emit('deleteitem', item);
+    });
+    socket.on('moveitem', (item, cell, sibling) => {
+        io.in(boardName).emit('moveitem', item, cell, sibling);
+    });
+    socket.on('addrow', (row, cells, header, sibling) => {
+        io.in(boardName).emit('addrow', row, cells, header, sibling);
+    });
+    socket.on('moverow', (row, sibling) => {
+        io.in(boardName).emit('moverow', row, sibling);
+    });
+    socket.on('deleterow', (row) => {
+        io.in(boardName).emit('deleterow', row);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
---
+++ public/script.js
@@ -0,0 +1,294 @@
+const numCells = 3;
+const boardName = window.location.pathname.split('/').filter(s => s)[0];
+
+const socket = io({
+    'query': {
+        'board': boardName
+    }
+});
+
+// Create element from HTML template.
+function create(templateId) {
+    const c = document.getElementById(templateId).content;
+    return c.cloneNode(true).children[0];
+};
+
+// Initialize drag and drop for all items in a row.
+function initDrag(row) {
+    const cells = row.querySelectorAll('.cell');
+    const cellsArray = Array.prototype.slice.call(cells);
+    const drake = dragula(cellsArray, {
+        direction: 'horizontal',
+        invalid: (el, handle) => {
+            return !(el.classList.contains('item') || el.classList.contains('textarea'));
+        },
+        accepts: (el, target, source, sibling) => {
+            return source.parentNode == target.parentNode;
+        }
+    });
+    drake.on('drop', (element, target, source, sibling) => {
+        emitMoveItem(element, target, sibling);
+    });
+};
+
+// Add an item before sibling to a cell. If id is null, a random
+// id is generated. If sibling is null, the item is appended to the end.
+function addItem(cell, id, sibling) {
+    const item = create('titem');
+    item.id = id ?? uuidv4();
+    cell.insertBefore(item, sibling);
+    return item;
+};
+
+// Updates the text content of an item.
+function updateItemContent(item, content) {
+    item.querySelector('.textarea').innerText = content;
+}
+
+// Change color of an item to color. Colors correspond to CSS classes
+// 'red', 'green' and 'blue'
+function changeColor(item, color) {
+    item.classList.remove('red');
+    item.classList.remove('green');
+    item.classList.remove('blue');
+    item.classList.add(color);
+};
+
+// Delete item from cell.
+function deleteItem(item) {
+    item.remove();
+};
+
+// Move item from current position to the position
+// before sibling in cell with an animation.
+function moveItem(item, cell, sibling) {
+    const oldX = item.getBoundingClientRect().x;
+    const oldY = item.getBoundingClientRect().y;
+    cell.insertBefore(item, sibling);
+    const newX = item.getBoundingClientRect().x;
+    const newY = item.getBoundingClientRect().y;
+    const dx = oldX - newX;
+    const dy = oldY - newY;
+    item.animate([
+        { transform: 'translate(' + dx + 'px,' + dy + 'px)' },
+        { transform: 'translate(0px, 0px)' }
+    ], {
+        duration: 200,
+        easing: 'ease-in'
+    });
+}
+
+// Add a row before sibling to the table. Automatically creates all cells
+// and a header item. The header item is added to the first column. If id,
+// cellIds, or headerItemId is null, random ids are generated. If sibling
+// is null, the row is appended to the end.
+function addRow(title, type, id, cellIds, headerItemId, sibling) {
+    const row = create('trow');
+    let newCellIds = [];
+    let newHeaderItemId = '';
+    row.id = id ?? uuidv4();
+    for (let i=0; i<numCells; ++i) {
+        const cell = create('tcell');
+        if (i == 0) {
+            const item = create('titemheader');
+            cell.classList.add('row-header');
+            item.classList.add('row-header');
+            item.classList.add(type ?? 'green');
+            item.id = headerItemId ?? uuidv4();
+            item.querySelector('.textarea').innerText = title ?? '';
+            cell.appendChild(item);
+            newHeaderItemId = item.id;
+        }
+        if (cellIds) {
+            cell.id = cellIds[i];
+        } else {
+            cell.id = uuidv4();
+            newCellIds.push(cell.id);
+        }
+
+        row.appendChild(cell);
+    }
+    initDrag(row);
+
+    const table = document.querySelector('.table');
+    table.insertBefore(row, sibling);
+
+    return [row, newCellIds, newHeaderItemId];
+}
+
+// Move row from current position to the position before
+// sibling with an animation.
+function moveRow(row, sibling) {
+    const table = document.querySelector('.table');
+    const oldX = row.getBoundingClientRect().x;
+    const oldY = row.getBoundingClientRect().y;
+    table.insertBefore(row, sibling);
+    const newX = row.getBoundingClientRect().x;
+    const newY = row.getBoundingClientRect().y;
+    const dx = oldX - newX;
+    const dy = oldY - newY;
+    row.animate([
+        { transform: 'translate(' + dx + 'px,' + dy + 'px)' },
+        { transform: 'translate(0px, 0px)' }
+    ], {
+        duration: 200,
+        easing: 'ease-in'
+    });
+}
+
+// Delete row from table.
+function deleteRow(row) {
+    row.remove();
+}
+
+// Send message to server.
+
+function emitAddItem(item, cell, sibling) {
+    socket.emit('additem', item.id, cell.id, sibling?.id);
+}
+
+function emitUpdateItemContent(item, content) {
+    socket.emit('updateitemcontent', item.id, content);
+}
+
+function emitUpdateItemColor(item, color) {
+    socket.emit('updateitemcolor', item.id, color);
+}
+
+function emitDeleteItem(item) {
+    socket.emit('deleteitem', item.id);
+}
+
+function emitMoveItem(item, cell, sibling) {
+    socket.emit('moveitem', item.id, cell.id, sibling?.id);
+}
+
+function emitAddRow(row, cells, header, sibling) {
+    socket.emit('addrow', row.id, cells, header, sibling?.id);
+}
+
+function emitDeleteRow(row) {
+    socket.emit('deleterow', row.id);
+}
+
+function emitMoveRow(row, sibling) {
+    socket.emit('moverow', row.id, sibling?.id);
+}
+
+// Receive messages from server.
+
+socket.on('additem', (itemId, cellId, siblingId) => {
+    const item = document.getElementById(itemId);
+    const cell = document.getElementById(cellId);
+    const sibling = document.getElementById(siblingId);
+    if (item) {
+        moveItem(item, cell, sibling);
+    } else {
+        addItem(cell, itemId, sibling);
+    }
+});
+
+socket.on('updateitemcontent', (itemId, content) => {
+    const item = document.getElementById(itemId);
+    if (item) {
+        updateItemContent(item, content);
+    }
+});
+
+socket.on('updateitemcolor', (itemId, color) => {
+    const item = document.getElementById(itemId);
+    if (item) {
+        changeColor(item, color);
+    }
+});
+
+socket.on('deleteitem', (itemId) => {
+    const item = document.getElementById(itemId);
+    if (item) {
+        deleteItem(item);
+    }
+});
+
+socket.on('moveitem', (itemId, cellId, siblingId) => {
+    const item = document.getElementById(itemId);
+    const cell = document.getElementById(cellId);
+    const sibling = document.getElementById(siblingId);
+    moveItem(item, cell, sibling);
+});
+
+socket.on('addrow', (rowId, cellIds, headerItemId, siblingId) => {
+    const row =  document.getElementById(rowId);
+    const sibling = document.getElementById(siblingId);
+    if (row) {
+        moveRow(row, sibling);
+    } else {
+        addRow(null, null, rowId, cellIds, headerItemId, sibling);
+    }
+});
+
+socket.on('moverow', (rowId, siblingId) => {
+    const row = document.getElementById(rowId);
+    const sibling = document.getElementById(siblingId);
+    if (row) {
+        moveRow(row, sibling);
+    }
+});
+
+socket.on('deleterow', (rowId) => {
+    const row = document.getElementById(rowId);
+    if (row) {
+        deleteRow(row);
+    }
+});
+
+// Event handlers bound in HTML.
+
+window.addRow = () => {
+    const [row, cellIds, headerItemId] = addRow();
+    row.querySelector('.textarea').focus();
+    emitAddRow(row, cellIds, headerItemId, row.nextElementSibling);
+};
+window.deleteRow = (el) => {
+    const row = el.closest('.row');
+    deleteRow(row);
+    emitDeleteRow(row);
+};
+window.addItem = (el) => {
+    const cell = el.closest('.cell');
+    const item = addItem(cell);
+    emitAddItem(item, cell, item.nextElementSibling);
+    item.querySelector('.textarea').focus();
+};
+window.editFinished = (el) => {
+    const item = el.closest('.item');
+    const content = item.querySelector('.textarea').innerText;
+    emitUpdateItemContent(item, content);
+};
+window.changeColor = (el, color) => {
+    const item = el.closest('.item');
+    changeColor(item, color);
+    emitUpdateItemColor(item, color);
+};
+window.deleteItem = (el) => {
+    const item = el.closest('.item');
+    deleteItem(item);
+    emitDeleteItem(item);
+};
+
+window.onload = () => {
+    const table = document.querySelector('.table');
+    const tableDrake = dragula([table], {
+        direction: 'vertical',
+        invalid: (el, handle) => {
+            return !handle.classList.contains('row');
+        },
+    });
+    tableDrake.on('drop', (element, target, source, sibling) => {
+        emitMoveRow(element, sibling);
+    });
+    
+    const rows = document.querySelectorAll('.row');
+    rows.forEach((row) => {
+        initDrag(row);
+    });
+}
